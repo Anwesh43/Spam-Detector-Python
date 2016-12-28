bc7f9986e39eef6ef9553526eb99959a4a22bd57
Anwesh43 <anweshthecool0@gmail.com>
2016-12-28 18:41:56 +0530

    Adding port for heroku

--- api.py
+++ api.py
@@ -1,10 +1,14 @@
 from flask import *
 from SpamDetector import *
+import os
 app = Flask(__name__)
 app.debug = True
 spamDetector = SpamDetector()
 spamDetector.train()
 spamDetector.test()
+port = 8080
+if os.environ.has_key('PORT'):
+    port = os.environ['PORT']
 @app.route('/test')
 def test():
     return "hello"
@@ -16,4 +20,4 @@ def detectspam():
     print result
     return 'This is {0}'.format(result)
 if __name__ == "__main__":
-    app.run()
+    app.run(port=port)
